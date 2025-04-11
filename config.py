24697ec7841c18d41604e299876f196accb8272e
Umaid <ahmedumaid707@gmail.com>
2025-04-11 21:51:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class Config(object):
     BOT_UPTIME  = time.time()
     START_PIC   = os.environ.get("START_PIC", "https://envs.sh/GGi.png?L9YiN=1")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5585016974').split()]
-    FORCE_SUB_1 = os.environ.get("FORCE_SUB_1", "anime_sub_society")
+    FORCE_SUB_1 = os.environ.get("FORCE_SUB_1", "animes_sub_society")
     FORCE_SUB_2 = os.environ.get("FORCE_SUB_2", "ongoiing_society")
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1002015354644))
     DUMP_CHANNEL = int(os.environ.get("DUMP_CHANNEL", "-1002015354644"))
